8b4696238a982f6358dd68f6a3f7962ff32caedd
Akshay Khalasi <akshay.khalasi3@gmail.com>
2022-10-15 18:26:42 -0700

    created react app, homepage still in progress

---
+++ frontend/mkbb-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from './Components/NavbarComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+        <NavbarComp></NavbarComp>
+    </div>
+  );
+}
+
+export default App;
