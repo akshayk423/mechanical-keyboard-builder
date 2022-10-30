3d77dc8ba5d2c629072362c3b6e0754a1f153d36
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-10-30 16:16:24 -0700

    Update SQL export

--- export.sql
+++ export.sql
@@ -16,6 +16,48 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `accessories`
+--
+
+DROP TABLE IF EXISTS `accessories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `accessories` (
+  `type` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accessories`
+--
+
+LOCK TABLES `accessories` WRITE;
+/*!40000 ALTER TABLE `accessories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accessories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `case`
+--
+
+DROP TABLE IF EXISTS `case`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `case` (
+  `size` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `case`
+--
+
+LOCK TABLES `case` WRITE;
+/*!40000 ALTER TABLE `case` DISABLE KEYS */;
+/*!40000 ALTER TABLE `case` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `keyboardParts`
 --
 
@@ -23,13 +65,13 @@ DROP TABLE IF EXISTS `keyboardParts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `keyboardParts` (
-  `partID` int NOT NULL,
+  `partID` int NOT NULL AUTO_INCREMENT,
   `URL` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
   `brand` varchar(45) NOT NULL,
-  `price` decimal(2,0) NOT NULL,
+  `price` int NOT NULL,
   PRIMARY KEY (`partID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +80,122 @@ CREATE TABLE `keyboardParts` (
 
 LOCK TABLES `keyboardParts` WRITE;
 /*!40000 ALTER TABLE `keyboardParts` DISABLE KEYS */;
+INSERT INTO `keyboardParts` VALUES (1,'www.google.com','name','brand',100);
 /*!40000 ALTER TABLE `keyboardParts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `keycaps`
+--
+
+DROP TABLE IF EXISTS `keycaps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `keycaps` (
+  `material` varchar(45) NOT NULL,
+  `profile` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `keycaps`
+--
+
+LOCK TABLES `keycaps` WRITE;
+/*!40000 ALTER TABLE `keycaps` DISABLE KEYS */;
+/*!40000 ALTER TABLE `keycaps` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `moderators`
+--
+
+DROP TABLE IF EXISTS `moderators`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `moderators` (
+  `modID` int NOT NULL AUTO_INCREMENT,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`modID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `moderators`
+--
+
+LOCK TABLES `moderators` WRITE;
+/*!40000 ALTER TABLE `moderators` DISABLE KEYS */;
+/*!40000 ALTER TABLE `moderators` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `partList`
+--
+
+DROP TABLE IF EXISTS `partList`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `partList` (
+  `partListID` int NOT NULL AUTO_INCREMENT,
+  `totalPrice` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`partListID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `partList`
+--
+
+LOCK TABLES `partList` WRITE;
+/*!40000 ALTER TABLE `partList` DISABLE KEYS */;
+/*!40000 ALTER TABLE `partList` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pcb`
+--
+
+DROP TABLE IF EXISTS `pcb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pcb` (
+  `hotSwappable` varchar(45) NOT NULL,
+  `size` varchar(45) NOT NULL,
+  `containsRGB` varchar(45) NOT NULL,
+  `connectionType` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pcb`
+--
+
+LOCK TABLES `pcb` WRITE;
+/*!40000 ALTER TABLE `pcb` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pcb` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prebuilt`
+--
+
+DROP TABLE IF EXISTS `prebuilt`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prebuilt` (
+  `switchName` varchar(45) NOT NULL,
+  `hotSwappable` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prebuilt`
+--
+
+LOCK TABLES `prebuilt` WRITE;
+/*!40000 ALTER TABLE `prebuilt` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prebuilt` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -61,6 +218,28 @@ CREATE TABLE `sellingList` (
 LOCK TABLES `sellingList` WRITE;
 /*!40000 ALTER TABLE `sellingList` DISABLE KEYS */;
 /*!40000 ALTER TABLE `sellingList` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stabilizers`
+--
+
+DROP TABLE IF EXISTS `stabilizers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stabilizers` (
+  `stabType` varchar(45) NOT NULL,
+  `info` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stabilizers`
+--
+
+LOCK TABLES `stabilizers` WRITE;
+/*!40000 ALTER TABLE `stabilizers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stabilizers` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -127,4 +306,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-30 15:18:42
+-- Dump completed on 2022-10-30 16:12:11
